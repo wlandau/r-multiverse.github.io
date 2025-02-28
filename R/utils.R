49ff3c7a3f23c24fb0ee7392f11d2a0735b2f8c2
wlandau <will.landau@gmail.com>
2025-02-28 08:57:16 -0500

    add fixes to better explain staging freeze

--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,7 @@
 snapshot_date <- function() {
+  if (!is.null(cache[["snapshot_date"]])) {
+    return(cache[["snapshot_date"]])
+  }
   path <- file.path(
     "https://raw.githubusercontent.com",
     "r-multiverse",
@@ -8,10 +11,31 @@ snapshot_date <- function() {
     "main",
     "date_snapshot.txt"
   )
-  readLines(url(path))
+  cache[["snapshot_date"]] <- readLines(url(path))
+  cache[["snapshot_date"]]
+}
+
+staging_date <- function() {
+  if (!is.null(cache[["staging_date"]])) {
+    return(cache[["staging_date"]])
+  }
+  path <- file.path(
+    "https://raw.githubusercontent.com",
+    "r-multiverse",
+    "production",
+    "refs",
+    "heads",
+    "main",
+    "date_staging_start.txt"
+  )
+  cache[["staging_date"]] <- readLines(url(path))
+  cache[["staging_date"]]
 }
 
 snapshot_r <- function() {
+  if (!is.null(cache[["snapshot_r"]])) {
+    return(cache[["snapshot_r"]])
+  }
   path <- file.path(
     "https://raw.githubusercontent.com",
     "r-multiverse",
@@ -21,5 +45,8 @@ snapshot_r <- function() {
     "main",
     "r_version_full.txt"
   )
-  readLines(url(path))
+  cache[["snapshot_r"]] <- readLines(url(path))
+  cache[["snapshot_r"]]
 }
+
+cache <- new.env(parent = emptyenv())
