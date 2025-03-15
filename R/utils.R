96746ffad2cfe8b1ef576bf95062277089301440
Will Landau <1580860+wlandau@users.noreply.github.com>
2025-03-15 06:20:07 -0400

    List most recent snapshots first utils.R

--- R/utils.R
+++ R/utils.R
@@ -10,6 +10,8 @@ snapshots <- function() {
   }
   url <- "https://production.r-multiverse.org/snapshots.json"
   snapshots <- jsonlite::stream_in(gzcon(url(url)), verbose = FALSE)
+  dates <- as.Date(snapshots$snapshot)
+  snapshots <- snapshots[rev(order(dates)), ]
   cache[["snapshots"]] <- snapshots
   cache[["snapshots"]]
 }
